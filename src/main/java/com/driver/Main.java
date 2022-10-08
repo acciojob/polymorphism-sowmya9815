897ea32bda495e367aa507bba53acc073dcafffd
sowmya9815 <sowmyaeedala9815@gmail.com>
2022-10-08 17:11:48 +0530

    overloading code donegit add .

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String args[]){
+        Product p = new Product();
+        int ans1 = p.product(3,4);
+        int ans2 = p.product(3,4,5);
+        double ans3 = p.product(3.0,5.0);
+        System.out.println(ans1);
+        System.out.println(ans2);
+        System.out.println(ans3);
+    }
+    static class Product{
+        public int product(int x, int y) {
+            return x+y;
+        }
+        public int product(int x, int y, int z) {
+            return x+y+z;
+        }
+        public double product(double x, double y) {
+            return x+y;
+        }
+    }
 }
