19fc61666b2b5e150900f5d0a683699dbcd2486c
sowmya9815 <sowmyaeedala9815@gmail.com>
2022-10-09 00:07:33 +0530

    final check

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Product p = new Product();
         int ans1 = p.product(3,4);
         int ans2 = p.product(3,4,5);
@@ -12,12 +12,15 @@ public class Main {
     }
     public static class Product{
         public int product(int x, int y) {
+
             return x+y;
         }
         public int product(int x, int y, int z) {
+
             return x+y+z;
         }
         public double product(double x, double y) {
+
             return x+y;
         }
     }
